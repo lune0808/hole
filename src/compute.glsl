3245ff0ab62ccaf01f8170d359b81b18fb08bb4c
lune <yagigami.goatgod@gmail.com>
2025-10-11 23:24:52 +0200

    draw circle in compute

--- src/compute.glsl
+++ src/compute.glsl
@@ -4,9 +4,20 @@ layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
 uniform layout(binding=0,rgba32f) writeonly restrict image2D screen;
 
 
+vec4 color(ivec2 coord)
+{
+	ivec2 off = coord - ivec2(400, 300);
+	int r = 200;
+	if (dot(off, off) < r * r) {
+		return vec4(0.0, 1.0, 1.0, 1.0);
+	} else {
+		return vec4(0.0);
+	}
+}
+
 void main()
 {
 	ivec2 coord = ivec2(gl_GlobalInvocationID.xy);
-	imageStore(screen, coord, vec4(1.0, 1.0, 0.0, 0.0));
+	imageStore(screen, coord, color(coord));
 }
 
