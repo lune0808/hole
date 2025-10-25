00f3442873bb62d2c83c31224126c029301f916d
lune <yagigami.goatgod@gmail.com>
2025-10-25 17:43:43 +0200

    add strafing animation script

--- script/strafe1.glsl
+++ script/strafe1.glsl
@@ -10,32 +10,33 @@ vec3 rotate_quat(vec4 q, vec3 v)
 	return v + 2.0 * cross(q.xyz, q.w * v + cross(q.xyz, v));
 }
 
-const float dt = 0.20;
+const float dt = 0.10;
+const uint iterations = 1024;
 
 void init()
 {
 	beg.q_orientation = quat(Y, 0.0);
-	end.q_orientation = quat(Y, PI/6.0);
+	end.q_orientation = quat(Y, 0.0);
 
-	beg.cam_pos = -4.0*X + 32.0*Z + 8.0*Y;
-	end.cam_pos = -4.0*X + 4.0*Z;
+	beg.cam_pos = 50.0*Z + 15.0*Y;
+	end.cam_pos = 50.0*Z + 15.0*Y;
 
-	beg.r_s = 2.0;
-	end.r_s = 2.0;
+	beg.r_s = 1.5;
+	end.r_s = 1.5;
 
-	beg.sphere_pos = 2.0*X - 2.0*Z;
-	end.sphere_pos = 2.0*X - 2.0*Z;
+	beg.sphere_pos = -20.0*X -2.0*Z;
+	end.sphere_pos = +20.0*X -2.0*Z;
 
 	beg.dt = dt;
 	end.dt = dt;
 
-	beg.iterations = 512;
-	end.iterations = 512;
+	beg.iterations = iterations;
+	end.iterations = iterations;
 
 	win.screen_width = 1280;
 	win.screen_height = 720;
-	win.n_frames = 128;
-	win.ms_per_frame = 33;
+	win.n_frames = 256;
+	win.ms_per_frame = 50;
 	win.skybox_id = SKYBOX_GENERIC;
 	win.fov = PI/3.0;
 
@@ -46,14 +47,14 @@ void init()
 	scene.accr_z = normalize(cross(accr_x, accr_y));
 	scene.accr_min_r = 4.5;
 	scene.accr_max_r = 35.0;
-	scene.accr_height = 3.5;
-	scene.accr_light = 0.65;
+	scene.accr_height = 2.0;
+	scene.accr_light = 2.00;
 	scene.accr_light2 = 0.85;
-	scene.accr_abso = 4.0;
+	scene.accr_abso = 0.7;
 
-	scene.red_exponent   = 1.0;
-	scene.green_exponent = 3.0;
-	scene.blue_exponent  = 9.0;
+	scene.red_exponent   = 1.2;
+	scene.green_exponent = 2.5;
+	scene.blue_exponent  = 7.0;
 }
 
 void loop()
